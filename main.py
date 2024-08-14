16f8025e3130eff059471a638f356035809b3ab6
GavFeng <gavfeng@uw.edu>
2024-08-14 03:20:03 +0000

    Gifs Working

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import discord
 from dotenv import load_dotenv
 import random
+import requests
 
 load_dotenv()
 
@@ -22,12 +23,23 @@ async def on_message(message):
     return
 
   if message.content.startswith('$based'):
-    tenor_gifs = [
-        'https://tenor.com/view/wonder-egg-priority-ai-ohto-egg-wonder-priority-gif-8174882731399849953',
-    ]
+    gif_url = 'https://media.tenor.com/cXMEiCWQJ-EAAAAC/wonder-egg-priority-ai-ohto.gif'
+    filename = 'based.gif'
 
-    random_gif = random.choice(tenor_gifs)
-    await message.channel.send(file=discord.File(url=random_gif))
+    # Download the file
+    def download_file(url, filename):
+      response = requests.get(url, stream=True)
+      with open(filename, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=1024):
+          if chunk:
+            f.write(chunk)
+
+    download_file(gif_url, filename)
+
+    # Create discord.File from the downloaded file
+    with open(filename, 'rb') as f:
+      based_gif = discord.File(f)
+      await message.channel.send(file=based_gif)
 
 
 token = os.getenv('TOKEN')
